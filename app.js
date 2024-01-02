9d52dc55464f9bf990f3bcc8e6606efb99f904fd
R1fky <rifkiyuda11@gmail.com>
2024-01-02 14:59:01 +0700

    menambahkan layouts

--- app.js
+++ app.js
@@ -1,23 +1,42 @@
-const express = require('express');
-const app = express()
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
 const port = 3000;
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
+app.use(expressLayouts);
 
-app.get('/', (req, res) => {
-    // res.sendFile('./index.html', {root: __dirname});
-    res.render('index', {
-        title: 'halaman index',
-        user: 'rifky Yudha',
-    });
+app.get("/", (req, res) => {
+  // res.sendFile('./index.html', {root: __dirname});
+  const mahasiswa = [
+    {
+      nama: "Rifky Yudha Pratama",
+      email: "rifky@gmail.com",
+    },
+    {
+      nama: "Andi Muhammad",
+      email: "muhammad@gmail.com",
+    },
+    {
+      nama: "Anjay",
+      email: "anjay@gmail.com",
+    },
+  ];
+  res.render("index", {
+    layout: "layouts/main-layouts",
+    title: "halaman index",
+    user: "rifky Yudha",
+    mahasiswa,
+  });
 });
 
-app.get('/about',(req, res) => {
-    res.render('about', {
-        title: 'halaman about'
-    })
+app.get("/about", (req, res) => {
+  res.render("about", {
+    layout: 'layouts/main-layouts',
+    title: "halaman about",
+  });
 });
 
 app.listen(port, () => {
-    console.log(`port dijalankan adalah http://localhost:${port}`)
+  console.log(`port dijalankan adalah http://localhost:${port}`);
 });
